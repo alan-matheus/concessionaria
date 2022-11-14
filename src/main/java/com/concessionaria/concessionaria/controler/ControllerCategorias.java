e935219d4bac973dbef30a279222a52a75389859
Alan Matheus <matheus10_11@hotmail.com>
2022-11-14 16:36:10 -0300

    atualizei
    editei o editar :D e o excluir de categorias

--- src/main/java/com/concessionaria/concessionaria/controler/ControllerCategorias.java
+++ src/main/java/com/concessionaria/concessionaria/controler/ControllerCategorias.java
@@ -2,10 +2,13 @@ package com.concessionaria.concessionaria.controler;
 
 
 import java.util.List;
+import java.util.Optional;
+
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -42,9 +45,37 @@ public class ControllerCategorias {
         mv.addObject("categoria", categoria);
 
         return mv;
+
+    }
+    
+    @RequestMapping (value = "/adm/categorias/remover/{id}", method = RequestMethod.GET)
+    public String deleteById(@PathVariable("id") int id, RedirectAttributes atributos){
+        repository.deleteById(id);
+        atributos.addFlashAttribute("mensagem", "deletado com sucesso");
+
+        return "redirect:/adm/categorias/lista";
     }
 
+    @RequestMapping (value = "/adm/categorias/update/{id}", method = RequestMethod.GET)
+    public ModelAndView update(@PathVariable("id") int id){
+        ModelAndView mv = new ModelAndView("adm/categorias/update");
+        Optional<Categoria> categorias = repository.findById(id);
+        mv.addObject("id_categoria", categorias.get().getId_categoria());
+        mv.addObject("nome", categorias.get().getNome());
+
+        return mv;
+    }
     
+    @RequestMapping (value = "adm/categorias/update/{id}", method = RequestMethod.POST)
+    public String updateCategoria(Categoria categoria){
+        Categoria agora = repository.findById(categoria.getId_categoria()).orElse(null);
+
+        agora.setNome(categoria.getNome());
+
+        repository.save(agora);
+
+        return "redirect:/adm/categorias/lista";
+    }
 
 
 
